6270e2425608d85ef5aa7dff9c98f90e7210979b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-02 15:49:05 -0300

    Apagando codigo anterior

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,24 +1,9 @@
 import './App.css';
 
-const BemVindo = (props) => {
-  return (
-    <>
-      <h2>Olá { props.nome }</h2>
-      <p>Você tem { props.idade } anos, certo?</p>
-    </>
-  );
-}
-
 function App() {
   return (
     <div>
-      <h1>Olá mundo!!</h1>
-      <p>Primeiro codigo com angular!</p>
-
-      <div>
-        <h1>Mensagem de Bem-Vindo</h1>
-        <BemVindo nome="Davyd" idade="18" />
-      </div>
+      
     </div>
   );
 }
