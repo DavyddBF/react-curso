1973af30649afa70ce7fe0652894eda9aac0d08c
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-02 17:10:34 -0300

    Adicionando o código sobre props e components
    Aqui entendi o que é e como funciona uma props e os componentes

--- props-component/src/App.js
+++ props-component/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Sobre = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Olá eu sou o(a) { props.nome }</h2>
+      <h3>Meu cargo é de { props.cargo }</h3>
+      <p>Tenho { props.idade } anos</p>
+    </div>
+  );
+}
+
+const RedeSocial = (props) => {
+  return (
+    <div>
+      <a href={props.fb}>Facebook -</a>
+      <a href={props.insta}> Instagram -</a>
+      <a href={props.yt}> Youtube</a>
+    </div>
+  );
+}
+
+const Equipe = (props) => {
+  return (
+    <div>
+      <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
+      <RedeSocial  fb={props.facebook} insta={props.instagram} yt={props.youtube} />
+      <hr></hr>
+    </div>
+  );
+}
+
+function App() {
+  const redesSociais = {
+    facebook: 'https://www.facebook.com/?locale=pt_BR',
+    instagram: 'https://www.instagram.com/',
+    youtube: 'https://www.youtube.com/'
+  }
+
+  return (
+    <div>
+      <h1>Conheça a equipe:</h1>
+      <Equipe nome="Davyd" cargo="estudante" idade="18" facebook={redesSociais.facebook} instagram={redesSociais.instagram} youtube={redesSociais.youtube} />
+      <Equipe nome="Rodrigo" cargo="designer" idade="28" facebook={redesSociais.facebook} instagram={redesSociais.instagram} youtube={redesSociais.youtube}/>
+      <Equipe nome="Pedro" cargo="programador" idade="16" facebook={redesSociais.facebook} instagram={redesSociais.instagram} youtube={redesSociais.youtube}/>
     </div>
   );
 }
