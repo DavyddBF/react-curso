0b15aceaae81e5286936a2a82e6331cf41892c8a
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-03 19:28:54 -0300

    chamando o componente Membro

--- eventos/src/App.js
+++ eventos/src/App.js
@@ -12,8 +12,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Olá mundo</h1>
-                <p>Eu sou <Membro /></p>
+                <Membro nome="Visitante" />
             </div>
         );
     }
