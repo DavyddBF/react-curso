b332be185b73d0db47efe6ce03f249a59e7acd71
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-03 15:28:20 -0300

    Criando um contador basico usando states

--- states/src/App.js
+++ states/src/App.js
@@ -5,14 +5,42 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nome: 'Davyd'
+      nome: 'Davyd',
+      contador: 0
     }
+	
+	this.aumentar = this.aumentar.bind(this);
+	this.diminuir = this.diminuir.bind(this);
   }
+  
+  aumentar() {
+	  let state = this.state;
+	  state.contador += 1;
+	  this.setState(state);
+  }
+  
+  diminuir() {
+	  let state = this.state;
+	  if(state.contador === 0) {
+      alert('Erro: Valor abaixo de ZERO');
+      return;
+	  } 
+    state.contador -= 1;
+		this.setState(state);
+  }
+	  
 
   render() {
     return (
       <div>
-        <p>Olá mundo, { this.state.nome }</p>
+        <h1>Contador</h1>
+      
+        <p>{ this.state.nome }</p>
+        <h2>
+          <button onClick={ this.diminuir }>-</button>
+            { this.state.contador }
+          <button onClick={ this.aumentar }>+</button>
+        </h2>
       </div>
     );
   }
