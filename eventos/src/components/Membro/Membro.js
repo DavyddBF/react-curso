8a3fafe5b6d6401a34e20d9e455cff155c389d5a
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-03 18:52:36 -0300

    Criando componente Membro

---
+++ eventos/src/components/Membro/Membro.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class Membro extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Membro</h1>
+      </div>
+    );
+  }
+}
+
+
+export default Membro;
