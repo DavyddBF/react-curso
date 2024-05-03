b1534fcf30d62ee170bc61a602011c5ec5379bd4
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-03 19:28:27 -0300

    Update index.js

--- eventos/src/components/Membro/index.js
+++ eventos/src/components/Membro/index.js
@@ -1,19 +1,28 @@
 import { Component } from 'react';
 
 class Membro extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            nome: props.nome
+        }
 
-  render() {
-    return (
-      <div>
-        <h1>Membro</h1>
-      </div>
-    );
-  }
+        this.entrar = this.entrar.bind(this);
+    }
+
+    entrar() {
+        this.setState({ nome: 'Davyd' });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Bem-vindo(a) {this.state.nome}</h1>
+                <button onClick={this.entrar}>Entrar Sistema</button>
+                <button onClick={() => this.setState({ nome: '' })}>Sair</button>
+            </div>
+        );
+    }
 }
-
 
 export default Membro;
