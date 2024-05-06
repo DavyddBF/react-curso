a9d45dfdc1ebfc7086ed2a721bc9be3013ca523d
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-06 15:35:47 -0300

    Update App.js

--- renderizacao-condicional/src/App.js
+++ renderizacao-condicional/src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 1
+    };
   }
 
   render() {
     return (
       <div>
+        {
+          this.state.status === 1 &&
+          <h1>Bem vindo ao sistema!!</h1>
+        }
+        <br/>
         <h1>Olá mundo</h1>
       </div>
     );
