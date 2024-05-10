2b7f810e0135e4c3887dbbd264f0748ebd4a059c
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-10 19:53:18 -0300

    Criando componente com classe

--- praticando-forms/src/App.js
+++ praticando-forms/src/App.js
@@ -1,2 +1,21 @@
 import { Component } from 'react';
 import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      titulo: "Olá mundo!!"
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{ this.state.titulo }</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
