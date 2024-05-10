64bda9be4817975ff1eea7ef4eeffbcf98f8ca9a
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-10 19:18:00 -0300

    Criando um form simples - Usando onChange

--- manipulando-forms/src/App.js
+++ manipulando-forms/src/App.js
@@ -5,14 +5,37 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state = {
-
+      email: 'email@email',
+      senha: ''
     }
+
+    this.trocaEmail = this.trocaEmail.bind(this);
+  }
+
+  trocaEmail(evento) {
+    const textoDigitado = evento.target.value;
+    this.setState({ email: textoDigitado });
   }
 
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Login</h1>
+        <br/>
+
+        <label>Email:</label>
+        <input type='email' name='email' value={this.state.email} onChange={ this.trocaEmail } />
+        <br/>
+        <br/>
+        <label>Senha:</label>
+        <input type='password' name='senha' value={this.state.senha} onChange={(evento) => {
+          const textoDigitado = evento.target.value;
+          this.setState({ senha: textoDigitado });
+        }} />
+
+        <div>
+          
+        </div>
       </div>
     );
   }
