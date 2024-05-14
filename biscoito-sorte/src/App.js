69d4a5698d37c2d5370d012f64eef74f8ebe0697
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-14 19:31:00 -0300

    Adicionando frases e criando componente botão

--- biscoito-sorte/src/App.js
+++ biscoito-sorte/src/App.js
@@ -4,15 +4,35 @@ import './App.css'
 class App extends Component{
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {};
+        this.frases = [
+            'Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.',
+            'O riso é a menor distância entre duas pessoas.', 
+            'Deixe de lado as preocupações e seja feliz.',
+            'Realize o óbvio, pense no improvável e conquiste o impossível.',
+            'Acredite em milagres, mas não dependa deles.',
+            'A maior barreira para o sucesso é o medo do fracasso.'
+        ];
     }
 
 
     render() {
         return (
-            <div>
-
+            <div className='container'>
+                <img className='img' src={require('./assets/biscoito.png')} />
+                <Botao/>
+                <h3 className='texto'>Frase</h3>
             </div>
+        );
+    }
+}
+
+class Botao extends Component {
+    render() {
+        return (
+            <>
+                <button>Abrir Biscoito</button>
+            </>
         );
     }
 }
