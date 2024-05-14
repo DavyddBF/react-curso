a2782f2322eee267bc1200c645edc0e14fe1da23
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-14 19:48:04 -0300

    Criando função que quebraBiscoito e adicionando evento

--- biscoito-sorte/src/App.js
+++ biscoito-sorte/src/App.js
@@ -4,7 +4,9 @@ import './App.css'
 class App extends Component{
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            frase: 'Clique no botão acima para gerar uma nova frase!!'
+        };
         this.frases = [
             'Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.',
             'O riso é a menor distância entre duas pessoas.', 
@@ -13,6 +15,13 @@ class App extends Component{
             'Acredite em milagres, mas não dependa deles.',
             'A maior barreira para o sucesso é o medo do fracasso.'
         ];
+
+        this.quebraBiscoito = this.quebraBiscoito.bind(this);
+    }
+
+    quebraBiscoito() {
+        let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+        this.setState({ frase: `" ${this.frases[numeroAleatorio]} "` }); 
     }
 
 
@@ -20,8 +29,8 @@ class App extends Component{
         return (
             <div className='container'>
                 <img className='img' src={require('./assets/biscoito.png')} />
-                <Botao/>
-                <h3 className='texto'>Frase</h3>
+                <Botao eventoOnClick={this.quebraBiscoito} nome="Abrir Biscoito" />
+                <h3 className='texto'>{this.state.frase}</h3>
             </div>
         );
     }
@@ -31,7 +40,7 @@ class Botao extends Component {
     render() {
         return (
             <>
-                <button>Abrir Biscoito</button>
+                <button onClick={this.props.eventoOnClick}>{this.props.nome}</button>
             </>
         );
     }
