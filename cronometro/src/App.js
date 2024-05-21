a81504026330635e1960034ff02a5740914a4716
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-21 19:41:29 -0300

    Criando componente com classe

---
+++ cronometro/src/App.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
