97cbc468a5214dc99b641a7efde6b516d42e933d
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-22 17:21:22 -0300

    Criando a lógica de iniciar e parar o cronometro

--- cronometro/src/App.js
+++ cronometro/src/App.js
@@ -5,29 +5,45 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tempo: 0
+      tempo: 0,
+      btnTitle: ['Vai', 'Limpar']
     };
 
+    this.timer = null;
     this.vai = this.vai.bind(this);
     this.limpar = this.limpar.bind(this);
   }
 
   vai() {
+    let state = this.state;
 
+    if(this.timer !== null) {
+      clearInterval(this.timer);
+      this.timer = null;
+      state.btnTitle = ['Vai', 'Limpar'];
+    } else {
+      this.timer = setInterval(() => {
+        state.tempo += 0.1;
+        this.setState(state);
+      }, 100)
+      this.state.btnTitle = ['Pausar', 'Limpar'];
+    }
+
+    this.setState(state);
   }
 
   limpar() {
-    
+
   }
 
   render() {
     return (
       <div className='container'>
         <img className='img' src={ require('./assets/cronometro.png') } />
-        <a className='timer'>0.0</a>
+        <a className='timer'>{ this.state.tempo.toFixed(1) }</a>
         <div className='area-btn'>
-          <Btn title='Vai'/>
-          <Btn title='Limpar' />
+          <Btn click={ this.vai } title={this.state.btnTitle[0]} />
+          <Btn click={ this.limpar } title={this.state.btnTitle[1]} />
         </div>
       </div>
     );
@@ -42,7 +58,7 @@ class Btn extends Component{
   render() {
     return (
       <>
-        <a className='btn'>{this.props.title}</a>
+        <a className='btn' onClick={ this.props.click }>{ this.props.title }</a>
       </>
     );
   }
