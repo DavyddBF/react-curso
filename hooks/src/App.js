0f18544d936550e5f4fbd74b7f52195f37020e9a
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-23 20:41:57 -0300

    Criando input

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -2,26 +2,31 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+
   const [tarefas, setTarefa] = useState([
     'Tarefa 1',
     'Tarefa 2'
   ]);
 
+  const [input, setInput] = useState('');
+
   function addTarefa() {
-    setTarefa([...tarefas, 'Ola mundo']);
+    setTarefa([...tarefas, input]);
+    setInput('');
   }
 
   return (
     <div>
       <ul>
         {
-          tarefas.map(tarefa => (
-            <li key={tarefa}>{tarefa}</li>
+          tarefas.map( tarefa => (
+            <li key={ tarefa }>{ tarefa }</li>
           ))
         }
       </ul>
 
-      <button onClick={ addTarefa }>Adicionar</button>
+      <input type='text' value={ input }  onChange={ evento => setInput(evento.target.value) } />
+      <button type='button' onClick={ addTarefa }>Adicionar</button>
     </div>
   );
 }
