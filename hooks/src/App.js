eb131c4b484c0a9587ccda2d4526ccc92d35ab6b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-23 20:34:03 -0300

    Criando uma lista usando states

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,9 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tarefas, setTarefa] = useState([
+    'Tarefa 1',
+    'Tarefa 2'
+  ]);
+
+  const [nome, setNome] = useState('Davyd');
+
   return (
     <div>
+      <ul>
+        {
+          tarefas.map(tarefa => (
+            <li key={tarefa}>{tarefa}</li>
+          ))
+        }
+      </ul>
 
+      <h1>{nome}</h1>
     </div>
   );
 }
