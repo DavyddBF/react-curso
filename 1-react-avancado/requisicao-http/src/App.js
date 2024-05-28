47745a70b5fb7e958df68e16052bc0786287e6cf
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-28 16:28:03 -0300

    Carregando api http

--- 1-react-avancado/requisicao-http/src/App.js
+++ 1-react-avancado/requisicao-http/src/App.js
@@ -2,6 +2,23 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [nutri, setNutri] = useState([]);
+
+  useEffect( () => {
+    function carregaApi() {
+      const api = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+      
+      fetch(api).then( resp => resp.json()).then( respJSON => {
+        console.log(respJSON);
+        setNutri(respJSON);
+      });
+    }
+
+    carregaApi();
+  }, []);
+
+
   return (
     <div className="container">
       <h1>Hello world</h1>
