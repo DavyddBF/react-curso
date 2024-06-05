df7884e4cab24f95794e6c5e4f25193b933e1ae5
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-05 16:53:38 -0300

    Criando componente

---
+++ 1-react-avancado/rotas/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+function Home() {
+
+    return (
+      <div>
+        <h1>Bem vindo ao Home</h1>
+      </div>
+    )
+  }
+  
+  export default Home;
---
+++ 1-react-avancado/rotas/src/pages/Sobre/index.tsx
@@ -0,0 +1,10 @@
+function Sobre() {
+
+    return (
+      <div>
+        <h1>Bem vindo ao Sobre</h1>
+      </div>
+    )
+  }
+  
+  export default Sobre;
