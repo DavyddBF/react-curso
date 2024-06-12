af0635df2b3c325e1f44239efa5904021eed3935
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 14:50:51 -0300

    Adicionando Link

--- 1-react-avancado/rotas/src/pages/Home/index.tsx
+++ 1-react-avancado/rotas/src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Home(): JSX.Element {
   return (
     <div className='container'>
       <h1>Bem vindo ao Home</h1>
+      
+      <ul>
+        <li>Ir para o <Link to='/produto/13'>produto 13</Link></li>
+      </ul>
     </div>
   )
 }
