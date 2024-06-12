d61b1e75ca1feeb18b20f136eb7b79709109b3f2
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 12:07:35 -0300

    Importando links de rotas

--- 1-react-avancado/rotas/src/pages/Home/index.tsx
+++ 1-react-avancado/rotas/src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Home(): JSX.Element {
   return (
     <div>
       <h1>Bem vindo ao Home</h1>
+
+      <Link to='/sobre'>Pagina Sobre</Link>
     </div>
   )
 }
--- 1-react-avancado/rotas/src/pages/Sobre/index.tsx
+++ 1-react-avancado/rotas/src/pages/Sobre/index.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Sobre(): JSX.Element {
   return (
     <div>
       <h1>Bem vindo ao Sobre</h1>
+
+      <Link to='/'>Pagina Home</Link>
     </div>
   )
 }
