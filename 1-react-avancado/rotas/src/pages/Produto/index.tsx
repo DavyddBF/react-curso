46960f7f92588514a63f75b6d8c2e61af9c0ea1b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 14:46:53 -0300

    Criando componente de produto

---
+++ 1-react-avancado/rotas/src/pages/Produto/index.tsx
@@ -0,0 +1,10 @@
+function Produto(): JSX.Element {
+    return (
+        <div>
+            <h2>Detalhes dos Produtos</h2>
+            <span>Meu produto: 123</span>
+        </div>
+    );
+}
+
+export default Produto;
