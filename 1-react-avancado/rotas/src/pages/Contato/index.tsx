bbdf25bc575edb8f86c45cb2fb31fbef0903a064
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 12:14:15 -0300

    Criando conteudo

--- 1-react-avancado/rotas/src/pages/Contato/index.tsx
+++ 1-react-avancado/rotas/src/pages/Contato/index.tsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 function Contato(): JSX.Element {
     return (
         <div>
-
+            <h1>Pagina de Contato</h1><br/>
+            <p>Numero da empresa (dd) x xxxx-xxxx</p>
+            <br/>
+            <br/>
+            
             <Link to='/'>Home</Link>
             <Link to='/sonbre'>Sobre</Link>
         </div>
