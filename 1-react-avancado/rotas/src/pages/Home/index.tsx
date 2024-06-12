2acadf485e5d4ccdf15883dfc7c630df71e9416d
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 12:14:22 -0300

    Pulando linha

--- 1-react-avancado/rotas/src/pages/Home/index.tsx
+++ 1-react-avancado/rotas/src/pages/Home/index.tsx
@@ -4,7 +4,9 @@ function Home(): JSX.Element {
   return (
     <div>
       <h1>Bem vindo ao Home</h1>
-
+      <br/>
+      <br/>
+      
       <Link to='/sobre'>Sobre</Link>
       <Link to='/contato'>Contato</Link>
     </div>
--- 1-react-avancado/rotas/src/pages/Sobre/index.tsx
+++ 1-react-avancado/rotas/src/pages/Sobre/index.tsx
@@ -4,6 +4,8 @@ function Sobre(): JSX.Element {
   return (
     <div>
       <h1>Bem vindo ao Sobre</h1>
+      <br/>
+      <br/>
 
       <Link to='/'>Home</Link>
       <Link to='/contato'>Contato</Link>
