baa8a27a38f686aad65d4ae5e70af0381642aa11
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 12:36:55 -0300

    Mudando tag

--- 1-react-avancado/rotas/src/components/Header/index.tsx
+++ 1-react-avancado/rotas/src/components/Header/index.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Header(): JSX.Element {
     return (
-        <div>
+        <header>
             <h2>OneRoute</h2>
 
             <Link to='/'>Home</Link>
             <Link to='/sobre'>Sobre</Link>
             <Link to='/contato'>contato</Link>
-        </div>
+        </header>
     );
 }
 
