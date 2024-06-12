2ac0ec89db77009ae336b8b4fb2ed560c8a16700
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 12:36:10 -0300

    Adicionando nome ao Header

--- 1-react-avancado/rotas/src/components/Header/index.tsx
+++ 1-react-avancado/rotas/src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 function Header(): JSX.Element {
     return (
         <div>
+            <h2>OneRoute</h2>
 
             <Link to='/'>Home</Link>
             <Link to='/sobre'>Sobre</Link>
