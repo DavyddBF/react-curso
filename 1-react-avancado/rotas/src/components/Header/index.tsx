69dbb9847e87cf173dd9e86c4cbb417b067e770c
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 14:01:35 -0300

    Adicionando uma div nos Links

--- 1-react-avancado/rotas/src/components/Header/index.tsx
+++ 1-react-avancado/rotas/src/components/Header/index.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import './style.css'
 
 function Header(): JSX.Element {
     return (
         <header>
             <h2>OneRoute</h2>
 
-            <Link to='/'>Home</Link>
-            <Link to='/sobre'>Sobre</Link>
-            <Link to='/contato'>contato</Link>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/sobre'>Sobre</Link>
+                <Link to='/contato'>contato</Link>    
+            </div>
         </header>
     );
 }
