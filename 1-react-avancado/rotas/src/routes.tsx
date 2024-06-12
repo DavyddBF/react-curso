3d19441d769724531b23bbb8374be6ccb514174b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-12 12:12:11 -0300

    Criando rota para contato

--- 1-react-avancado/rotas/src/routes.tsx
+++ 1-react-avancado/rotas/src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
+import Contato from './pages/Contato';
 
 function Rotas(): JSX.Element {
     return(
@@ -9,6 +10,7 @@ function Rotas(): JSX.Element {
             <Routes>
                 <Route path='/' element={ <Home/> } />
                 <Route path='/sobre' element={ <Sobre/> } />
+                <Route path='/contato' element={ <Contato/> } />
             </Routes>
         </BrowserRouter>
     );
