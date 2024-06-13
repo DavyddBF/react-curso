7d2c50cd9fac4d9453dfc7b86f355286de1963cc
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-13 09:29:05 -0300

    Criando function de Error

---
+++ 2-projetos/one-flix/src/pages/Error/index.tsx
@@ -0,0 +1,17 @@
+
+
+function Error(): JSX.Element {
+    return (
+        <div>
+            <h1>Página não encontrada!!</h1>
+            <br/>
+            <br/>
+            <h3>Segue os seguintes links encontrados:</h3>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Error;
