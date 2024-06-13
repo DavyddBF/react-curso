4cb08ae9a72dfbe42030651d325e39ff28753cc1
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-13 10:08:23 -0300

    Criando Header

--- 2-projetos/one-flix/src/components/Header/index.tsx
+++ 2-projetos/one-flix/src/components/Header/index.tsx
@@ -1,9 +1,11 @@
-
+import { Link } from "react-router-dom";
+import './header.css';
 
 function Header(): JSX.Element {
     return (
-        <div>
-            <h2>OneFlix</h2>
+        <div className="header">
+            <h2><Link className="header__logo" to='/'>OneFlix</Link></h2>
+            <Link className="header__favoritos" to='/favoritos'>Meus Filmes</Link>
         </div>
     );
 }
