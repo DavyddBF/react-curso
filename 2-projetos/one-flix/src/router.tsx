de81fbb73630a308cc083ce7931b490494709cfd
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-13 10:08:41 -0300

    Adicionando Header as Rotas

--- 2-projetos/one-flix/src/router.tsx
+++ 2-projetos/one-flix/src/router.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Paginas
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 import Favoritos from "./pages/Favoritos";
-
 import Error from "./pages/Error";
+
+// Componentes
+import Header from "./components/Header";
+
 
 function RouterApp(): JSX.Element {
     return (
         <BrowserRouter>
+            <Header/>
             <Routes>
                <Route path="/" element={ <Home/> } />
                <Route path="/favoritos" element={ <Favoritos/> } />
