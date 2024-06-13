0ac192af8b94b906030ba3533de17a95521b18b0
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-13 09:36:32 -0300

    Adicionando a uma rota a pagina de Erro

--- 2-projetos/one-flix/src/router.tsx
+++ 2-projetos/one-flix/src/router.tsx
@@ -3,12 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 
+import Error from "./pages/Error";
+
 function Rotas(): JSX.Element {
     return (
         <BrowserRouter>
             <Routes>
                <Route path="/" element={ <Home/> } /> 
                <Route path="/filme/:id" element={ <Filme/> } /> 
+
+               <Route path="*" element={ <Error/> } />
             </Routes>
         </BrowserRouter>
     );
