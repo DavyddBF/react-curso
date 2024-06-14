aceebd2e8a8c5326b77356aa09812a51c6c04990
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-14 10:57:44 -0300

    Criando logica que traz os dados da api

--- 2-projetos/one-flix/src/pages/Home/index.tsx
+++ 2-projetos/one-flix/src/pages/Home/index.tsx
@@ -1,6 +1,25 @@
-
+import { useEffect, useState } from "react";
+import api from "../../services/api";
 
 function Home(): JSX.Element {
+    const [filmes, setFilmes] = useState();
+
+    useEffect( () => {
+        async function carregaApi() {
+            const resposta = await api.get('movie/now_playing', {
+                params: {
+                    api_key: '184ee90db92bf2330058b37260977a7f',
+                    language: 'pt-BR',
+                    page: 1
+                }
+            });
+
+            console.log(resposta);
+        }
+
+        carregaApi();
+    }, []);
+
     return (
         <div>
             <h1>Bem vindo ao Home</h1>
