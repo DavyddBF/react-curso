4cd25fcf7c72d996a52e3e57222ad640d5fa451b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-15 11:05:01 -0300

    Criado function do footer

---
+++ 2-projetos/one-flix/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+function Footer(): JSX.Element {
+    return (
+        <div className='footer'>
+            <p className='footer__descricao'>Todos o direitos reservados a @DavyddBF</p>
+        </div>
+    );
+}
+
+export default Footer;
