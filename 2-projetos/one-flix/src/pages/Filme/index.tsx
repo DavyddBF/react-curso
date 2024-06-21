f50a4f62b21107b7e51d370356ec159fcf8ec5ab
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-21 16:10:47 -0300

    Tela loading | Criando conteudo

--- 2-projetos/one-flix/src/pages/Filme/index.tsx
+++ 2-projetos/one-flix/src/pages/Filme/index.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import api from "../../services/api";
+import FilmeType from "../../filmes";
 
 import './filme.css';
 
 function Filme(): JSX.Element {
     const { id }= useParams();
+    const [filme, setFilme] = useState<FilmeType>({});
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         async function carregaApi() {
@@ -16,18 +19,32 @@ function Filme(): JSX.Element {
                     language: 'pt-BR'
                 }
             }).then((resposta) => {
-                console.log(resposta);
-            }).catch((erro) => {
-                console.log(erro);
+                setFilme(resposta.data);
+                setLoading(false);
+            }).catch(() => {
+                console.log("Erro");
             });
         }
 
         carregaApi();
+
+        return () => {
+            console.log('Componente desconstruido');
+        }
     }, []);
+
+    if(loading) {
+        return (
+            <div className="loading">
+                <h1>Carregando...</h1>
+            </div>
+        );
+    }
 
     return (
         <div className="container">
-            <h1>Acessar filme com id: { id }</h1>
+            <h1>{ filme.title }</h1>
+            <img src={ `https://image.tmdb.org/t/p/original/${filme.backdrop_path}` } alt="" />
         </div>
     );
 }
