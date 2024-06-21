54b0297304b53a39397d26bbd273407fb2fc39ab
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-21 15:40:35 -0300

    Importando && Trazendo a api

--- 2-projetos/one-flix/src/pages/Filme/index.tsx
+++ 2-projetos/one-flix/src/pages/Filme/index.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+import api from "../../services/api";
+
+import './filme.css';
 
 function Filme(): JSX.Element {
+    const { id }= useParams();
+
+    useEffect(() => {
+        async function carregaApi() {
+            await api.get(`movie/${ id }`, {
+                params: {
+                    api_key: '184ee90db92bf2330058b37260977a7f',
+                    language: 'pt-BR'
+                }
+            }).then((resposta) => {
+                console.log(resposta);
+            }).catch((erro) => {
+                console.log(erro);
+            });
+        }
+
+        carregaApi();
+    }, []);
+
     return (
-        <div>
-            <h1>Pagina de detalhes dos Filmes</h1>
+        <div className="container">
+            <h1>Acessar filme com id: { id }</h1>
         </div>
     );
 }
