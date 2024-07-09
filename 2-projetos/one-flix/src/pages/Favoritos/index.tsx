1cadf6aa87a0f5500dff7374661c2a7617feeda2
DavyddBF <davydextreme460@gmail.com>
2024-07-09 15:07:34 -0300

    Fazendo imports

--- 2-projetos/one-flix/src/pages/Favoritos/index.tsx
+++ 2-projetos/one-flix/src/pages/Favoritos/index.tsx
@@ -1,9 +1,11 @@
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import './favoritos.css';
 
 function Favoritos(): JSX.Element {
     return (
         <div>
-            <h1>Filmes Favoritos</h1>
+            <h1>Filmes Favoritos:</h1>
         </div>
     );
 }
