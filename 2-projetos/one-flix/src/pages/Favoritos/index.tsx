a52d12d27626a678609ca6e64fb5a1cb22432a94
DavyddBF <davydextreme460@gmail.com>
2024-07-09 15:36:56 -0300

    Criando a lógica de mostrar filmes adicionados

--- 2-projetos/one-flix/src/pages/Favoritos/index.tsx
+++ 2-projetos/one-flix/src/pages/Favoritos/index.tsx
@@ -1,11 +1,33 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import FilmeType from "../../filmes";
 import './favoritos.css';
 
 function Favoritos(): JSX.Element {
+    const [filme, setFilme] = useState<FilmeType[]>([]);
+
+    useEffect(() => {
+        const listaFilme: string | null = localStorage.getItem('@oneflix');
+        setFilme(listaFilme ? JSON.parse(listaFilme) : []);
+    }, []);
+
     return (
-        <div>
+        <div className="meus__filmes">
             <h1>Filmes Favoritos:</h1>
+
+            <ul>
+                {filme.map((filme) => {
+                    return (
+                        <li key={ filme.id }>
+                            <span>{ filme.title }</span>
+                            <div>
+                                <Link to={`/filme/${ filme.id }`}>Ver detalhes</Link>
+                                <button>Excluir</button>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
         </div>
     );
 }
