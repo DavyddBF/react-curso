5e23352508e7bd22cf16b9adcb017b55ab913e48
DavyddBF <davydextreme460@gmail.com>
2024-07-09 11:50:06 -0300

    Criando opção de busca do trailer no yt

--- 2-projetos/one-flix/src/pages/Filme/index.tsx
+++ 2-projetos/one-flix/src/pages/Filme/index.tsx
@@ -59,7 +59,7 @@ function Filme(): JSX.Element {
                 <div className="area__btn">
                     <button>Salvar</button>
                     <button>
-                        <a href="">Trailer</a>
+                        <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${ filme.title } Trailer`}>Trailer</a>
                     </button>
                 </div>
             </div>
