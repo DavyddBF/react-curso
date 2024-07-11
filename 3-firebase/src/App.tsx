5a686c55bdb284cad7f008664537241a79857aec
DavyddBF <davydextreme460@gmail.com>
2024-07-11 15:11:41 -0300

    Criando jsx base

--- 3-firebase/src/App.tsx
+++ 3-firebase/src/App.tsx
@@ -1,9 +1,24 @@
+import { db } from './firebase/firebaseConnection';
 import './App.css';
 
 function App(): JSX.Element {
+    function adicionar() {
+        alert('Olá mundo')
+    }
+
     return (
         <div>
             <h1>Firebase + React</h1>
+
+            <div className='container'>
+                <label>Titulo:</label>
+                <input type='text' placeholder='Digite o titulo' />
+
+                <label>Autor:</label>
+                <input type="text" placeholder='Autor do post' />
+
+                <button onClick={ adicionar }>Cadastrar</button>
+            </div>
         </div>
     );
 }
