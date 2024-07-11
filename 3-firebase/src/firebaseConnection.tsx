81b3a625da99b3afa54036056c26486b20b489c6
DavyddBF <davydextreme460@gmail.com>
2024-07-11 14:40:16 -0300

    Código esboço de confi do firebase

--- 3-firebase/src/firebaseConnection.tsx
+++ 3-firebase/src/firebaseConnection.tsx
@@ -1,1 +1,20 @@
 // Arquivo de esboço para infos do firebase!!
+
+
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//     apiKey: "Sua apiKey aqui",
+//     authDomain: "Seu authDomain aqui",
+//     projectId: "Seu projectId aqui",
+//     storageBucket: "Seu storageBucket aqui",
+//     messagingSenderId: "Seu messagingSenderId aqui",
+//     appId: "Seu appId aqui",
+//     measurementId: "Seu measurementId aqui"
+// };
+
+// const firebaseApp = initializeApp(firebaseConfig);
+// const db = getFirestore(firebaseApp);
+
+// export { db };
