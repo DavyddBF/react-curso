9dbb995b773cca37ddec08d453af0c3d4ead034c
DavyddBF <davydextreme460@gmail.com>
2024-07-12 17:14:37 -0300

    Importando auth no esboço do firebase

--- 3-firebase/src/firebaseConnection.tsx
+++ 3-firebase/src/firebaseConnection.tsx
@@ -1,30 +1,34 @@
-// Arquivo de esboço para infos do firebase!!
+/*
+    [ Arquivo de esboço para infos do firebase!! ]
 
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { Firestore, getFirestore } from "firebase/firestore";
+import { Auth, getAuth } from "firebase/auth";
 
-// import { FirebaseApp, initializeApp } from "firebase/app";
-// import { Firestore, getFirestore } from "firebase/firestore";
+interface firebaseConfig {
+    apiKey: string,
+    authDomain: string,
+    projectId: string,
+    storageBucket: string,
+    messagingSenderId: string,
+    appId: string,
+    measurementId: string
+}
 
-// interface firebaseConfig {
-//     apiKey: string,
-//     authDomain: string,
-//     projectId: string,
-//     storageBucket: string,
-//     messagingSenderId: string,
-//     appId: string,
-//     measurementId: string
-// }
+const firebaseConfig: firebaseConfig = {
+    apiKey: "Sua apiKey aqui",
+    authDomain: "Seu authDomain aqui",
+    projectId: "Seu projectId aqui",
+    storageBucket: "Seu storageBucket aqui",
+    messagingSenderId: "Seu messagingSenderId aqui",
+    appId: "Seu appId aqui",
+    measurementId: "Seu measurementId aqui"
+};
 
-// const firebaseConfig: firebaseConfig = {
-//     apiKey: "Sua apiKey aqui",
-//     authDomain: "Seu authDomain aqui",
-//     projectId: "Seu projectId aqui",
-//     storageBucket: "Seu storageBucket aqui",
-//     messagingSenderId: "Seu messagingSenderId aqui",
-//     appId: "Seu appId aqui",
-//     measurementId: "Seu measurementId aqui"
-// };
+const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
+const db: Firestore = getFirestore(firebaseApp);
+const auth: Auth = getAuth(firebaseApp);
 
-// const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
-// const db: Firestore = getFirestore(firebaseApp);
+export { db, auth };
 
-// export { db };
+*/
