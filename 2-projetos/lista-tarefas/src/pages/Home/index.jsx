7cf9f4a5d7e2109276bb1414340e8544e8a3dbca
DavyddBF <davydextreme460@gmail.com>
2024-07-26 19:05:56 -0300

    Criando formulário de acesso

--- 2-projetos/lista-tarefas/src/pages/Home/index.jsx
+++ 2-projetos/lista-tarefas/src/pages/Home/index.jsx
@@ -1,8 +1,34 @@
+import { useState } from 'react';
 import './home.css';
 
 function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>Aqui é a Home</div>
+    <div className='home-container'>
+      <h1>Lista de Tarefas</h1>
+      <span>Gerencie suas tarefas de uma forma prática</span>
+
+      <form>
+        <input 
+          type="text"
+          placeholder="Digite seu email..."
+          value={ email }
+          onChange={ (event) => setEmail(event.target.value) }
+        />
+
+        <input
+          autoComplete="false"
+          type="password"
+          placeholder="********"
+          value={ password }
+          onChange={ (event) => setPassword(event.target.value) }
+        />
+
+        <button type="submit">Acessar</button>
+      </form>
+    </div>
   );
 }
 
