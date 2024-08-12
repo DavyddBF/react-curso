e9efeb89f80ffb86228ddf78237fe379ea514c54
DavyddBF <davydextreme460@gmail.com>
2024-08-12 16:40:27 -0300

    Criando o register

--- 2-projetos/lista-tarefas/src/pages/Register/index.jsx
+++ 2-projetos/lista-tarefas/src/pages/Register/index.jsx
@@ -1,8 +1,47 @@
-import './register.css';
+import { useState } from 'react';
+
+import { Link } from "react-router-dom";
 
 function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleCadastrar(event) {
+    event.preventDefault();
+
+    if(email !== '' && password !== '') {
+      alert('Teste');
+    } else {
+      alert('Preencha todos os campos!!');
+    }
+  }
+
   return (
-    <div>Pagina de Registro</div>
+    <div className='home-container'>
+      <h1>Cadastrar</h1>
+      <span>Poucos passos de ter sua própria lista de tarefas</span>
+
+      <form className='home-form' onSubmit={ handleCadastrar }>
+        <input 
+          type="text"
+          placeholder="Digite seu email..."
+          value={ email }
+          onChange={ (event) => setEmail(event.target.value) }
+        />
+
+        <input
+          autoComplete="false"
+          type="password"
+          placeholder="********"
+          value={ password }
+          onChange={ (event) => setPassword(event.target.value) }
+        />
+
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      <Link className='button-link' to='/'>Já possui uma conta? Faça o seu login!</Link>
+    </div>
   );
 }
 
