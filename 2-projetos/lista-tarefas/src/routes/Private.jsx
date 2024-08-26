a7a52f8e986dbe657d8a6756c2642e30417485b1
DavyddBF <davydextreme460@gmail.com>
2024-08-26 18:12:23 -0300

    Criando recurso que verifica se o user está logado

--- 2-projetos/lista-tarefas/src/routes/Private.jsx
+++ 2-projetos/lista-tarefas/src/routes/Private.jsx
@@ -1,3 +1,44 @@
+import { useState, useEffect } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { Navigate } from "react-router-dom";
+
 export default function Private({ children }) {
+    const [loading, setLoading] = useState(true);
+    const [signed, setSigned] = useState(false);
+
+    useEffect(() => {
+        async function checaLogin() {
+            const unsub = onAuthStateChanged(auth, user => {
+                if(user) {
+                    const userData = {
+                        uid: user.uid,
+                        email: user.email
+                    }
+
+                    localStorage.setItem('@detalheUser', JSON.stringify(userData));
+
+                    setLoading(false);
+                    setSigned(true);
+                } else {
+                    setLoading(false);
+                    setSigned(false);
+                }
+            })
+
+            return () => unsub();
+        }
+
+        checaLogin();
+    }, []);
+
+    if(loading) {
+        return <></>
+    }
+
+    if(!signed) {
+        return <Navigate to='/' />
+    }
+
     return children;
 }
