3210cbd07c1cde1e1ade78e78b0f045db311f19b
DavyddBF <davydextreme460@gmail.com>
2024-09-09 15:48:07 -0300

    Criando btn de logout e header

--- 2-projetos/lista-tarefas/src/pages/Admin/index.jsx
+++ 2-projetos/lista-tarefas/src/pages/Admin/index.jsx
@@ -1,8 +1,29 @@
+import { signOut } from 'firebase/auth';
 import './admin.css';   
+import { auth } from '../../firebase';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Admin() {
+    const navigate = useNavigate();
+    
+    async function logOut() {
+        await signOut(auth)
+        .then(
+            navigate('/', { replace: true })
+        );
+    }
+
     return (
-        <div>Admin</div>
+        <div>
+            <header className='adm-header'>
+                <div>
+                    <h1><Link to='/'>Dashboard</Link></h1>
+                    <button onClick={ logOut }>
+                        LogOut
+                    </button>
+                </div>
+            </header>
+        </div>
     );
 }
 
